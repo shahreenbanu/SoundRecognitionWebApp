95657e875537464f5c4c723c7366c0cf4461f001
shahreenbanu <85785868+shahreenbanu@users.noreply.github.com>
2022-10-26 16:17:21 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,43 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/pM9EERNXb/model.json", modelReady);
+
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
+function gotResults(error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+
+
+        document.getElementById("result_label").innerHTML= "I can hear - "+results[0].label;
+        document.getElementById("result_confidence").innerHTML="Accuracy -"+ (results[0].confidence * 100).toFixed(2)+ " %";
+        document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+    document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        img1 = document.getElementById("img")
+
+        if (results[0].label == Background-Noise) {
+           
+            img1.src="ear-clipart-5.jpg";
+            
+        }
+        elseif (results[0].label == Dog);{
+
+            
+            img1.src="cartoon-happy-little-dog-vector-23038445.jpg";
+            
+        }
+   elseif (results[0].label == Cat);{
+img1.src="Cute Lovely Staring Kitten Cat iPhone 8 Wallpapers.jpg";
+  }
+            
+}
+    }
+
